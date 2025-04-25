3d1553df002feeff786653cffcfba83d56709e6f
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2025-04-25 20:30:12 +0700

    create, read, search data

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -10,10 +10,15 @@ class TodoController extends Controller
 {
     public function index()
     {
-        $todos = Todo::where('user_id', Auth::id())->get();
-        dd($todos);
-        return view('todo.index');
+        $todos = Todo::where('user_id', auth()->user()->id)
+            ->orderBy('is_done', 'asc')
+            ->orderBy('created_at', 'desc')
+            ->get();
+            // dd($todos);
+
+        return view('todo.index', compact('todos'));
     }
+
 
     public function create()
     {
@@ -23,5 +28,25 @@ class TodoController extends Controller
     public function edit()
     {
         return view('todo.edit');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+        ]);
+
+        $todo = todo::create([
+            'title' => ucfirst($request->title),
+            'user_id' => Auth::id(),
+        ]);
+        return redirect()->route('todo.index')->with('success', 'todo created successfully.');
+    }
+
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+
+        return redirect()->route('todo.index')->with('success', 'todo deleted successfully.');
     }
 }
---
+++ tugas5.md
@@ -0,0 +1,18 @@
+# Tugas 5
+
+## 1. Todo Create
+
+![alt text](screenshot/tugas5/todo-create.png)
+
+## 2. Todo Created
+
+![alt text](screenshot/tugas5/todo-created.png)
+
+## 3. User Search
+
+![alt text](screenshot/tugas5/user-searchh.png)
+
+## 4. User Search Result
+
+![alt text](screenshot/tugas5/user-search-result.png)
+
