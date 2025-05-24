f0d6ae6edcf69b8768a53de6d38362777c14f271
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2025-05-24 15:57:35 +0700

    mailtrap

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function() {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -59,6 +59,10 @@ Route::middleware('auth','admin')->group(function () {
     Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin'])->name('user.makeadmin');
     Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeadmin'])->name('user.removeadmin');
 });
+
+
+
+
 
 
 require __DIR__.'/auth.php';
